b2a1b4b974fcaf7a1ea16eea9527a5f13e6b4d73
Asher Shoshan <asher.shoshan@gmail.com>
2018-12-23 15:49:59 +0200

    master - 23/12

---
+++ src/test/java/hello/AsherJUnitTest.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author tmeltse
+ */
+public class AsherJUnitTest
+{
+	public AsherJUnitTest()
+	{
+	}
+
+	@BeforeClass
+	public static void setUpClass()
+	{
+	}
+
+	@AfterClass
+	public static void tearDownClass()
+	{
+	}
+
+	@Before
+	public void setUp()
+	{
+	}
+
+	@After
+	public void tearDown()
+	{
+	}
+
+	@Test
+	public void test4Asher()
+	{
+		try
+		{
+			URL asher = new URL("http://10.106.161.40:31613");
+			URLConnection yc = asher.openConnection();
+			try (BufferedReader in = new BufferedReader(new InputStreamReader(yc.getInputStream())))
+			{
+				String inputLine;
+				StringBuilder sb = new StringBuilder();
+				
+				while ((inputLine = in.readLine()) != null)
+				{
+					sb.append(inputLine);
+				}
+				
+				assertEquals("No match!","Greetings from Spring Boot!",sb.toString());
+			}
+		}
+		catch (MalformedURLException ex)
+		{
+			Logger.getLogger(AsherJUnitTest.class.getName()).log(Level.SEVERE, null, ex);
+		}
+		catch (IOException ex)
+		{
+			Logger.getLogger(AsherJUnitTest.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
+}
