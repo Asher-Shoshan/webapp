450b58d6755a4d09137588649facde9db22f7de4
Asher Shoshan <asher.shoshan@gmail.com>
2018-12-23 19:29:58 +0200

    feature1 - 23/12 19.30

--- src/test/java/hello/HelloControllerTest.java
+++ src/test/java/hello/HelloControllerTest.java
@@ -27,5 +27,6 @@ public class HelloControllerTest {
         mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().string(equalTo("Greetings from Spring Boot! - Feature1")));
+                // rem
     }
 }
